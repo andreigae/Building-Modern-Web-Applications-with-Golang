f95a0544ab04dc6bf99bef309b7f0212e3eacc9b
Andrei GM <hola@andreigae.com>
2023-02-18 22:27:52 +0100

    14. Loops and ranging over data

--- main.go
+++ main.go
@@ -3,56 +3,20 @@ package main
 import "log"
 
 func main() {
-	// the if statement
-	var isTrue bool
-
-	isTrue = false
-
-	if isTrue == true {
-		log.Println("isTrue is", isTrue)
-	} else {
-		log.Println("isTrue is", isTrue)
+	type User struct {
+		FirstName string
+		LastName  string
+		Email     string
+		Age       int
 	}
 
-	cat := "cat2"
+	var users []User
+	users = append(users, User{"John", "Smith", "john@smith.com", 30})
+	users = append(users, User{"Mary", "Jones", "mary@jones.com", 20})
+	users = append(users, User{"Sally", "Brown", "sally@smith.com", 45})
+	users = append(users, User{"Alex", "Anderson", "alex@smith.com", 17})
 
-	if cat == "cat" {
-		log.Println("Cat is cat")
-	} else {
-		log.Println("Cat is not cat")
-	}
-
-	myNum := 100
-	isTrue = false
-
-	if myNum > 99 && !isTrue {
-		log.Println("myNum is greater than 99 and isTrue is set to true")
-	} else if myNum < 100 && isTrue {
-		// do something
-	} else if myNum == 101 || isTrue {
-		// do something
-	} else if myNum > 1000 && isTrue {
-		// do something
-	}
-
-	// the switch statement
-
-	myVar := "cat"
-
-	switch myVar {
-	case "cat":
-		log.Println("myVar is set to cat")
-
-	case "dog":
-		log.Println("myVar is set to dog")
-
-	case "horse":
-		log.Println("myVar is set to horse")
-
-	case "fish":
-		log.Println("myVar is set to fish")
-
-	default:
-		log.Println("myVar is something else")
+	for _, l := range users {
+		log.Println(l.FirstName, l.LastName, l.Email, l.Age)
 	}
 }
