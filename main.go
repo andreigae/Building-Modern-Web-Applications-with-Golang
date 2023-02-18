a2ea886c1a79e756e9473d58e2b809ab901d5a44
Andrei GM <hola@andreigae.com>
2023-02-18 22:26:16 +0100

    12. Other data structures: Maps and Slices

--- main.go
+++ main.go
@@ -1,24 +1,48 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
-type myStruct struct {
+type User struct {
 	FirstName string
-}
-
-func (m *myStruct) printFirstName() string {
-	return m.FirstName
+	LastName  string
 }
 
 func main() {
-	var myVar myStruct
+	// maps
+	myMap := make(map[string]User)
 
-	myVar.FirstName = "John"
-
-	myVar2 := myStruct{
-		FirstName: "Mary",
+	me := User{
+		FirstName: "Trevor",
+		LastName:  "Sawler",
 	}
 
-	log.Println("myVar is set to", myVar.printFirstName())
-	log.Println("myVar2 is set to", myVar2.printFirstName())
+	myMap["me"] = me
+
+	log.Println(myMap["me"].FirstName)
+
+	// slices
+	var mySlice []int
+
+	mySlice = append(mySlice, 2)
+	mySlice = append(mySlice, 1)
+	mySlice = append(mySlice, 3)
+
+	sort.Ints(mySlice)
+
+	log.Println(mySlice)
+
+	// shorthand for slices
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	log.Println(numbers)
+
+	// print first two elements of slice, starting at first position
+	log.Println(numbers[0:2])
+
+	// create a slice of strings
+	names := []string{"one", "seven", "fish", "cat"}
+	log.Println(names)
 }
