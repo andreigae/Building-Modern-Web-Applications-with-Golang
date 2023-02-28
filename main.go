96c56ca132751523c82a07d4adc099a0cf925c93
Andrei Gae Matei <andrei@macrofunnels.com>
2023-02-28 22:16:07 +0100

    11. Receivers: Structs with functions

--- main.go
+++ main.go
@@ -1,65 +1,58 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"time"
-)
+import "log"
 
-// Variables globales (disponibles para todas las funciones del paquete)
-var s string = "Hello World, this is a global variable"
-
-// Definir un tipo de dato personalizado -> "User", que tendra los siguientes campos:
-// FirstName, LastName, PhoneNumber, Age, BirthDate
-
-// PD: Se podria evitar crear esta estrucrura y crear las variables de forma individual:
-// var FirstName string, var LastName string, etc. Pero esto no es muy escalable
-// si tenemos que pasar muchas variables a una funcion, por ejemplo, tenemos que pasar
-// x num de variables, y si en el futuro tenemos que pasar (x num + 20) variables, tendriamos que cambiar
-// la funcion y agregar mas variables, lo cual no es muy escalable.
-
-// SI en cambio, creamos una estructura, podemos pasar la estructura completa
-// y no tendriamos que cambiar la funcion, solo la estructura (Tipo de dato personalizado).
-
-type User struct {
-	// Los parametros estan en Mayusuclas para que sean publicos,
-	// si los ponemos en minusculas, solo seran accesibles dentro del paquete (package) VER EJEMPLO 1
-	FirstName   string
-	LastName    string
-	PhoneNumber string
-	Age         int
-	BirthDate   time.Time
+type myStruct struct {
+	FirstName string
 }
 
-func main() {
-
-	// Acceder a la variable global s, e Imprimir el valor de la variable
-	log.Println(s)
-
-	// Crear una variable de tipo User y asignarle valores a sus campos
-	user := User{
-		FirstName:   "Trevor",
-		LastName:    "Sawler",
-		PhoneNumber: "1 555 555 1212",
-	}
-
-	// Imprimir los valores de los campos de la variable user
-	// Ver la diferencia entre usar log y fmt
-	fmt.Println(user.FirstName, user.LastName, "Birthdate:", user.BirthDate)
+// Se crea una funcion printFirstName() dentro de la estructura myStruct
+// esta funcion regresa el valor del campo FirstName de la estructura
+func (myStr *myStruct) printFirstName() string {
+	return myStr.FirstName
 }
 
 /*
-*
+PD: Normalmente una funcion se declara como:
 
-EJEMPLO 1:
-func ThisIsAPublicFunction() {
-	Esta funcion es publica, porque esta en mayuscula
-	 sera accesible desde cualquier parte del programa
+func printFirstName() string {
+
 }
 
-func thisIsAPrivateFunction() {
-	 Esta funcion es privada, porque esta en minuscula,
-	 solo sera accesible dentro del paquete (package)
-}
-*
+Pero eso es una funcion sin mas, que no pertenece a ninguna estructura.
+
+Podriamos hacer que llamando a esta funcion se imprima el valor del campo de interes
+pero no es lo mismo que hacerlo dentro de la estructura, ya que la funcion necesita recibir
+la estructura como parametro, y eso no es lo que queremos
+
+Lo que quieremos es que la funcion sea parte de la estructura, y que no necesite recibir nada
+=> Que sea parte de la estructura y que devuelva su campo FirstName.
+
+Por eso  (myStr *myStruct), es lo que se denomina un reciver,
+que viene a decirle a la funcion que pertenece a una estructura y por ende puede acceder
+a sus campos a traves de la variable myStr
+
 */
+
+func main() {
+	// Declarar una variable de tipo myStruct
+	var myVar myStruct
+
+	// Establecer el valor del campo FirstName a "John"
+	myVar.FirstName = "John"
+
+	// Declara una variable de tipo myStruct y
+	// Establece el valor del campo FirstName a "Mary"
+	myVar2 := myStruct{
+		FirstName: "Mary",
+	}
+
+	// Imprime el valor de los campos FirstName de las variables
+	log.Println("myVar is set to", myVar.FirstName)
+	log.Println("myVar2 is set to", myVar2.FirstName)
+
+	// Se crea una funcion printFirstName() dentro de la estructura myStruct,
+	// esta funcion regresa el valor del campo FirstName de la estructura
+	log.Println("myVar is set to", myVar.printFirstName())
+	log.Println("myVar2 is set to", myVar2.printFirstName())
+}
