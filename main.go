e77ae3dfe7b2aa722965e262ff3cd522c4429bd8
Andrei Gae Matei <andrei@macrofunnels.com>
2023-02-28 22:40:55 +0100

    12. Other data structures: Maps and Slices

--- main.go
+++ main.go
@@ -1,58 +1,92 @@
 package main
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
-type myStruct struct {
+type User struct {
 	FirstName string
-}
-
-// Se crea una funcion printFirstName() dentro de la estructura myStruct
-// esta funcion regresa el valor del campo FirstName de la estructura
-func (myStr *myStruct) printFirstName() string {
-	return myStr.FirstName
+	LastName  string
 }
 
 /*
-PD: Normalmente una funcion se declara como:
+Esta es una forma sencilla de trabajar con los datos en Go
+pero hay otras formas de hacerlo que sirven para diferentes situaciones
 
-func printFirstName() string {
+func main() {
+	var myString string
+	var MyInt int
+	myString = "Hello World,"
+	MyInt = 42
 
+	mySecondString := "Hello World 2,"
+
+	log.Println(myString, mySecondString, MyInt)
 }
-
-Pero eso es una funcion sin mas, que no pertenece a ninguna estructura.
-
-Podriamos hacer que llamando a esta funcion se imprima el valor del campo de interes
-pero no es lo mismo que hacerlo dentro de la estructura, ya que la funcion necesita recibir
-la estructura como parametro, y eso no es lo que queremos
-
-Lo que quieremos es que la funcion sea parte de la estructura, y que no necesite recibir nada
-=> Que sea parte de la estructura y que devuelva su campo FirstName.
-
-Por eso  (myStr *myStruct), es lo que se denomina un reciver,
-que viene a decirle a la funcion que pertenece a una estructura y por ende puede acceder
-a sus campos a traves de la variable myStr
-
 */
 
 func main() {
-	// Declarar una variable de tipo myStruct
-	var myVar myStruct
 
-	// Establecer el valor del campo FirstName a "John"
-	myVar.FirstName = "John"
+	/*************************** MAPS ***************************/
 
-	// Declara una variable de tipo myStruct y
-	// Establece el valor del campo FirstName a "Mary"
-	myVar2 := myStruct{
-		FirstName: "Mary",
+	// Los mapas funcionan como arrays en otros lenguajes...
+	// Son una colección de datos que se almacenan en pares clave-valor,
+	// Funcionan muy muy rapido y son inmutales, no se pueden modificar,
+	// PD: no se pueden ordenar para obtener valores por indice como en los arrays
+	// array[0] array[1] => se sortena automaticamente y lo que pensabas guardabas
+	// en el el indice 0, es el indice 1 o 3, etc
+
+	myMap1 := make(map[string]string)
+	myMap1["dogName"] = "Lola"
+	myMap1["dogAge"] = "4"
+
+	log.Println(myMap1["dogName"])
+	log.Println(myMap1["dogAge"])
+
+	// Un mapa que contiene un struct (User)
+	myMap := make(map[string]User)
+
+	// Crear un nuevo struct con los datos de un usuario
+	me := User{
+		FirstName: "Trevor",
+		LastName:  "Sawler",
 	}
 
-	// Imprime el valor de los campos FirstName de las variables
-	log.Println("myVar is set to", myVar.FirstName)
-	log.Println("myVar2 is set to", myVar2.FirstName)
+	// Guardar el struct en el mapa con la clave "me"
+	myMap["me"] = me
 
-	// Se crea una funcion printFirstName() dentro de la estructura myStruct,
-	// esta funcion regresa el valor del campo FirstName de la estructura
-	log.Println("myVar is set to", myVar.printFirstName())
-	log.Println("myVar2 is set to", myVar2.printFirstName())
+	log.Println(myMap["me"].FirstName)
+
+	/*************************** SLICE 1 ***************************/
+
+	// slices -> arrays dinamicos, agregar valores a un array
+	// pero sin saber cuantos valores se agregran
+
+	// Inicio el slice vacio (el array dinamico)
+	var mySlice []int
+
+	// Le agrego 3 valores o los que quiera 2, 1, 3
+	mySlice = append(mySlice, 2)
+	mySlice = append(mySlice, 1)
+	mySlice = append(mySlice, 3)
+
+	// Ordeno el slice de menor a mayor
+	sort.Ints(mySlice)
+
+	// Imprimo el slice
+	log.Println(mySlice)
+
+	/*************************** SLICE 2 ***************************/
+
+	// Hay otra forma de crear un slice (Array dinamico)):
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	log.Println(numbers)
+
+	// print first two elements of slice, starting at first position
+	log.Println(numbers[0:2])
+
+	// create a slice of strings
+	names := []string{"one", "seven", "fish", "cat"}
+	log.Println(names)
 }
