334f6e967bac792fc4cb8fe13273c1d2506c14ce
Andrei Gae Matei <andrei@macrofunnels.com>
2023-02-28 22:03:07 +0100

    10. Types and Structs

--- main.go
+++ main.go
@@ -1,23 +1,65 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+// Variables globales (disponibles para todas las funciones del paquete)
+var s string = "Hello World, this is a global variable"
+
+// Definir un tipo de dato personalizado -> "User", que tendra los siguientes campos:
+// FirstName, LastName, PhoneNumber, Age, BirthDate
+
+// PD: Se podria evitar crear esta estrucrura y crear las variables de forma individual:
+// var FirstName string, var LastName string, etc. Pero esto no es muy escalable
+// si tenemos que pasar muchas variables a una funcion, por ejemplo, tenemos que pasar
+// x num de variables, y si en el futuro tenemos que pasar (x num + 20) variables, tendriamos que cambiar
+// la funcion y agregar mas variables, lo cual no es muy escalable.
+
+// SI en cambio, creamos una estructura, podemos pasar la estructura completa
+// y no tendriamos que cambiar la funcion, solo la estructura (Tipo de dato personalizado).
+
+type User struct {
+	// Los parametros estan en Mayusuclas para que sean publicos,
+	// si los ponemos en minusculas, solo seran accesibles dentro del paquete (package) VER EJEMPLO 1
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
+}
 
 func main() {
-	var myString string
-	myString = "Green"
 
-	log.Println("myString is set to", myString)
-	// se pide a la funcion changeUsingPointer que cambie el valor de myString
-	// pasandole la direccion de memoria de myString (un puntero)
-	changeUsingPointer(&myString)
+	// Acceder a la variable global s, e Imprimir el valor de la variable
+	log.Println(s)
 
-	// Despues de llamar a la funcion, myString deberia ser Red
-	log.Println("after func call myString is set to", myString)
+	// Crear una variable de tipo User y asignarle valores a sus campos
+	user := User{
+		FirstName:   "Trevor",
+		LastName:    "Sawler",
+		PhoneNumber: "1 555 555 1212",
+	}
+
+	// Imprimir los valores de los campos de la variable user
+	// Ver la diferencia entre usar log y fmt
+	fmt.Println(user.FirstName, user.LastName, "Birthdate:", user.BirthDate)
 }
 
-// Esta funcion recibe un puntero a un string y lo cambia de Green a Red
-func changeUsingPointer(s *string) {
-	log.Println("s is set to", s)
-	newValue := "Red"
-	*s = newValue
+/*
+*
+
+EJEMPLO 1:
+func ThisIsAPublicFunction() {
+	Esta funcion es publica, porque esta en mayuscula
+	 sera accesible desde cualquier parte del programa
 }
+
+func thisIsAPrivateFunction() {
+	 Esta funcion es privada, porque esta en minuscula,
+	 solo sera accesible dentro del paquete (package)
+}
+*
+*/
