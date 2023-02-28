37eac953f9ee6ad4121ea5355dd14f65c92f1bd3
Andrei Gae Matei <andrei@macrofunnels.com>
2023-02-28 21:14:46 +0100

    8. Variables & Functions

--- main.go
+++ main.go
@@ -15,6 +15,17 @@ func main() {
 
 	fmt.Println("i is set to", i)
 
+	// Guardar LOS RESULTADOS de la función en variables (whatWasSaid, theOtherThingThatWasSaid)
+	// GO permite retornar multiples valores
+	// En este caso, la función retorna dos strings
+	// Lo que hace esta linea realmente es, ejecutar la funcion saySometing()
+	// y sea lo que sea que devuelve esa función, lo guarda en las variables
+
+	// PD: La funcion esta tipificada para retornar dos strings por lo que
+	// no se puede guardar en variables otro tipo de informacion, ya que de por si
+	// la funcion esta obligada a devolver 2 strings y ante cualqueir otro error
+	// no copila directamente el programa
+
 	whatWasSaid, theOtherThingThatWasSaid := saySomething()
 
 	fmt.Println("The function returned", whatWasSaid, theOtherThingThatWasSaid)
