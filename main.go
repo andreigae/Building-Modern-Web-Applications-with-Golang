8b73f2411ebe34fe2708969022023069471baf54
Andrei Gae Matei <andrei@macrofunnels.com>
2023-03-01 17:11:36 +0100

    15. Interfaces

--- main.go
+++ main.go
@@ -1,82 +1,71 @@
 package main
 
-import "log"
+import "fmt"
+
+// Animal defines the interface for type Animal
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+// Dog defines the dog type
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+// Gorilla defines the Gorilla type
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
 
 func main() {
-
-	// En go para hacer iteraciones solo se usa el "for"
-	// Para hacer un for clasico se usa la siguiente sintaxis
-
-	for i := 0; i < 10; i++ {
-		log.Println(i)
+	dog := Dog{
+		Name:  "Samson",
+		Breed: "German Shepherd",
 	}
 
-	// Tambien podrias declarar primero la variable luego solo usar el for
-	/*
-		*
-		var i2 int
-		for i2 = 0; i2 < 10; i2++ {
-			log.Println(i2)
-		}
-		*
-	*/
+	PrintInfo(&dog)
 
-	animales := []string{"Perro", "Gato", "Conejo", "Pez", "Tortuga"}
-
-	// Iterar sobre un slice
-	// i2 es el indice del elemento, animal es el valor de cada uno de los elementos
-	// esta variable se declara y se asigna automaticamente por cada iteracion
-	// Por eso se declara con :=
-	for i2, animal := range animales {
-		log.Println(i2, animal)
+	gorilla := Gorilla{
+		Name:          "Jock",
+		Color:         "grey",
+		NumberOfTeeth: 38,
 	}
 
-	// Si no quiero el indice puedo usar el guion bajo _ para esa variable
-	// NO SE PUEDE OMITIR LA VARIABLE DEL INDICE SIN MAS,
-	// Ya que en vez de devolver el valor del ememento, devolveria el indice
-	// Pensando que el valor no se esta usando
-	for _, animal := range animales {
-		log.Println(animal)
-	}
+	PrintInfo(&gorilla)
+}
 
-	// Iterar sobre un map
-	abuelos := make(map[string]string)
-	abuelos["abuelaName"] = "Mary"
-	abuelos["abueloName"] = "John"
+// PrintInfo recibe un valor que es de tipo Animal (la interfaz)
+// Y esta interfaz tiene dos métodos: Says() y NumberOfLegs(),
+// Por lo que el valor que recibe PrintInfo debe tener esos dos métodos
 
-	for id, value := range abuelos {
-		log.Println(id, ":", value)
-	}
+// Para ello se debe agregar a la estructura Dog y Gorilla
+// los métodos Says() y NumberOfLegs()
 
-	// Iterar sobre un string
-	var str string = "Hola Mundo"
+func PrintInfo(a Animal) {
+	fmt.Println("This animal says", a.Says(), "and has", a.NumberOfLegs(), "legs")
+}
 
-	for i, c := range str {
-		log.Println(i, ":", string(c))
-	}
-	// Se usa el string(c) para convertir el caracter a string y poder imprimirlo
-	// ya que el caracter es un tipo de dato int32, y se trabaja con la tabla ASCII
+// Agregar los métodos Says() y NumberOfLegs() a la estructura Dog y Gorilla
 
-	// Crear un tipo de dato personalizado "User"
-	type User struct {
-		FirstName string
-		LastName  string
-		Email     string
-		Age       int
-	}
+// Podria omitirse el uso de punteros en los métodos de la interfaz,
+// pero es buena práctica usarlos para evitar copias de la estructura etc.
 
-	// Crear un slice de tipo "User" (Array Dinamico)
-	var users []User
+func (d *Dog) Says() string {
+	return "Woof"
+}
 
-	// Agregarle 4 elementos al slice
-	users = append(users, User{"John", "Smith", "john@smith.com", 30})
-	users = append(users, User{"Mary", "Jones", "mary@jones.com", 20})
-	users = append(users, User{"Sally", "Brown", "sally@smith.com", 45})
-	users = append(users, User{"Alex", "Anderson", "alex@smith.com", 17})
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
 
-	// Iterar sobre el slice
-	for _, user := range users {
-		// Imprimir los datos de cada elemento del slice
-		log.Println(user.FirstName, user.LastName, user.Email, user.Age)
-	}
+func (d *Gorilla) Says() string {
+	return "Ugh"
+}
+
+func (d *Gorilla) NumberOfLegs() int {
+	return 2
 }
