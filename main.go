16514f277b611b6337683e52fd4d5fbcce7ce192
Andrei Gae Matei <andrei@macrofunnels.com>
2023-03-01 16:31:40 +0100

    13. Decision Structures

--- main.go
+++ main.go
@@ -1,92 +1,65 @@
 package main
 
-import (
-	"log"
-	"sort"
-)
-
-type User struct {
-	FirstName string
-	LastName  string
-}
-
-/*
-Esta es una forma sencilla de trabajar con los datos en Go
-pero hay otras formas de hacerlo que sirven para diferentes situaciones
+import "log"
 
 func main() {
-	var myString string
-	var MyInt int
-	myString = "Hello World,"
-	MyInt = 42
+	// the if statement
+	var isTrue bool
 
-	mySecondString := "Hello World 2,"
+	// declarar una varialbe y asignarle el valor de false
+	isTrue = false
 
-	log.Println(myString, mySecondString, MyInt)
-}
-*/
-
-func main() {
-
-	/*************************** MAPS ***************************/
-
-	// Los mapas funcionan como arrays en otros lenguajes...
-	// Son una colección de datos que se almacenan en pares clave-valor,
-	// Funcionan muy muy rapido y son inmutales, no se pueden modificar,
-	// PD: no se pueden ordenar para obtener valores por indice como en los arrays
-	// array[0] array[1] => se sortena automaticamente y lo que pensabas guardabas
-	// en el el indice 0, es el indice 1 o 3, etc
-
-	myMap1 := make(map[string]string)
-	myMap1["dogName"] = "Lola"
-	myMap1["dogAge"] = "4"
-
-	log.Println(myMap1["dogName"])
-	log.Println(myMap1["dogAge"])
-
-	// Un mapa que contiene un struct (User)
-	myMap := make(map[string]User)
-
-	// Crear un nuevo struct con los datos de un usuario
-	me := User{
-		FirstName: "Trevor",
-		LastName:  "Sawler",
+	// Si is true es igual a true
+	if isTrue == true {
+		// Imprimir en consola el valor de isTrue
+		log.Println("isTrue is", isTrue)
+	} else {
+		// Si no es igual a true, imprimir en consola el valor de isTrue
+		log.Println("isTrue is", isTrue)
 	}
 
-	// Guardar el struct en el mapa con la clave "me"
-	myMap["me"] = me
+	// Esta forma de declarar una variable y asignarle un valor es mas comun
+	// (Se declara y se agigna el valor en la misma linea)
 
-	log.Println(myMap["me"].FirstName)
+	cat := "cat"
 
-	/*************************** SLICE 1 ***************************/
+	if cat == "cat" {
+		log.Println("Cat is cat")
+	} else {
+		log.Println("Cat is not cat")
+	}
 
-	// slices -> arrays dinamicos, agregar valores a un array
-	// pero sin saber cuantos valores se agregran
+	myNum := 100
+	isTrue = false
 
-	// Inicio el slice vacio (el array dinamico)
-	var mySlice []int
+	if myNum > 99 && !isTrue {
+		log.Println("myNum is greater than 99 and isTrue is set to true")
+	} else if myNum < 100 && isTrue {
+		// do something
+	} else if myNum == 101 || isTrue {
+		// do something
+	} else if myNum > 1000 && isTrue {
+		// do something
+	}
 
-	// Le agrego 3 valores o los que quiera 2, 1, 3
-	mySlice = append(mySlice, 2)
-	mySlice = append(mySlice, 1)
-	mySlice = append(mySlice, 3)
+	// the switch statement
 
-	// Ordeno el slice de menor a mayor
-	sort.Ints(mySlice)
+	myVar := "cat"
 
-	// Imprimo el slice
-	log.Println(mySlice)
+	switch myVar {
+	case "cat":
+		log.Println("myVar is set to cat")
 
-	/*************************** SLICE 2 ***************************/
+	case "dog":
+		log.Println("myVar is set to dog")
 
-	// Hay otra forma de crear un slice (Array dinamico)):
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	log.Println(numbers)
+	case "horse":
+		log.Println("myVar is set to horse")
 
-	// print first two elements of slice, starting at first position
-	log.Println(numbers[0:2])
+	case "fish":
+		log.Println("myVar is set to fish")
 
-	// create a slice of strings
-	names := []string{"one", "seven", "fish", "cat"}
-	log.Println(names)
+	default:
+		log.Println("myVar is something else")
+	}
 }
